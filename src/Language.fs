0e5c043b87f48afa15a0d8640ea971b787fc6cc7
Joseph DiSalvo <jdisalvo@projetech.com>
2020-04-19 11:47:06 -0400

    more language

--- src/Language.fs
+++ src/Language.fs
@@ -14,27 +14,30 @@ module Language =
         | Absolute of x: int * y: int
         | Operated of origin: Point * operation: Operation
         | OnSegment of PointOnSegment
-        static member toSegment (p1, p2) = LinkPoints(p1, p2)
+        | Intersection of Line * Line
+        static member toSegment p1 = Singlet(p1)
         static member expression p1 = PointExp(p1)
 
     and PointOnSegment =
         | PointOnSegment of position: double * segment: Segment
 
     and Segment = 
-        | LinkPoints of ``from``: Point * ``to``: Point
+        | Singlet of Point
         | Perpendicular of  position: double * originSegment: Segment * endSegment: Segment
-        | Chain of ``from``: Segment * ``to``: Point
         | Concat of Segment * Segment
         | QuadraticBezier of ``from``: Point * ``to``: Point * control: Point
         | Snipped of original: Segment * cutAt: Segment
-        static member toSegment (s, p2) = Chain(s, p2)
+        static member toSegment s = s
         static member expression s = SegmentExp(s)
+        static member toLine s = ExtendSegment(s)
 
     // Lines are inifinite and Segments are finite
     and Line =
         | Perpendicular of position: double * segment: Segment
-        | X of xPosition: double * segment: Segment
-        | Y of yPosition: double * segment: Segment
+        | AtX of xPosition: double * segment: Segment
+        | AtY of yPosition: double * segment: Segment
+        | ExtendSegment of Segment
+        static member toLine l = l
 
     and Operation =
         | Translate
@@ -54,28 +57,27 @@ module Language =
         | PolygonExp of Polygon
         | VarExp of name: string * Expression 
 
-
     let up location segment = Up <| PointOnSegment(location, segment)
     let down location segment = Down <| PointOnSegment(location, segment)
-    let (++) segment location = up location segment
-    let (--) segment location = down location segment
 
+    let inline asSegment (p:^t) = (^t: (static member toSegment: ^t -> Segment) (p))
+    let inline (+) p q = Concat(asSegment p, asSegment q)
 
-    let inline (=>) (p:^t) (q:Point) = (^t: (static member toSegment: ^t * Point -> Segment) (p, q))
-    let (+) s1 s2= Concat(s1, s2)
+    let inline asLine (x:^t) = (^t: (static member toLine: ^t -> Line) (x))
+    let inline (*) x y = Intersection(asLine x, asLine y)
 
-    // let At position segment = PointOnSegment(position, segment)
-    // TODO: remember that this needs to take directionality into account somehow
     let (@) segment position = OnSegment(PointOnSegment(position, segment))
 
     let (-|) segment position = Line.Perpendicular(position, segment)
     let (-|>) segment (position, endSegment) = Segment.Perpendicular(position, segment, endSegment)
-    // let T segment position = Line.Perpendicular(position, segment)
-    // let T2 segment position endSegment = Segment.Perpendicular(position, segment, endSegment)
+
+    let X position segment = AtX(position, segment)
+    let Y position segment = AtY(position, segment)
 
     let (|-|) orig cutAt = Snipped(orig, cutAt)
 
     let (%) p r = Operated(p, r)
+
 
     let inline expression (x:^t) = (^t: (static member expression: ^t -> Expression) (x))
     let var name x = VarExp(name, expression x)
@@ -101,7 +103,7 @@ module PrimitiveCells =
     type Builder<'a> = 'a -> Polygon list * Expression list
 
 
-module Example = 
+module Examples = 
     open Language
     open PrimitiveCells
 
@@ -109,20 +111,20 @@ module Example =
     let buildWildOne:Builder<Square> = fun square ->
         let {TopLeft =a; TopRight= b; BottomRight=c; BottomLeft=d;} = square
 
-        let i1 = (a => b) -|> (0.25, d => c) @ 0.25 
-        let i2 = (b => a) -|> (0.33, c => d) @ 0.33
+        let i1 = a + b -|> (0.25, d + c) @ 0.25 
+        let i2 = b + a -|> (0.33, c + d) @ 0.33
 
         let r1 = Rotate(C4, d)
         let r2 = Rotate(C4, b)
 
-        let leftBorder = i2 % r2 => b => i2 => c => i1 % r1
-        let rightBorder = i2 % r2 => a => i1 => b => i1 % r1
+        let leftBorder = (i2 % r2) + b + i2 + c + (i1 % r1)
+        let rightBorder = (i2 % r2) + a + i1 + b + (i1 % r1)
         let border = leftBorder + rightBorder
 
-        let focal = (a => d) -|> (0.5, leftBorder) @ 2.0
+        let focal = a + d -|> (0.5, leftBorder) @ 2.0
 
-        let downFrom at = focal => leftBorder @ at |-| rightBorder |> down 0.5
-        let upFrom at = focal => leftBorder @ at |-| rightBorder |> up 0.5
+        let downFrom at = focal + (leftBorder @ at) |-| rightBorder |> down 0.5
+        let upFrom at = focal + (leftBorder @ at) |-| rightBorder |> up 0.5
 
         let p1 = downFrom <| 1.0/6.0
         let p2 = downFrom <| 2.0/6.0
@@ -131,24 +133,28 @@ module Example =
         let p5 = downFrom <| 5.0/6.0
         let p6 = upFrom <| 5.0/6.0
 
-        ([p1; p2; p3; p4; p5; p6], [var "i1" i1; var "i2" i2])
+        ([p1; p2; p3; p4; p5; p6], [var "i1" i1; var "i2" i2; var "i1 % r1" <| i1 % r1; var "i2 % r2" <| i2 % r2])
 
     // Designing Tessellations p176
     let buildSoaringHigh:Builder<IsoscelesTriangle> =  fun triangle ->
+        // a
+        // |\
+        // |   \
+        // |     \
+        // |       \  b
+        // |      /
+        // |    /
+        // |  /
+        // |/
+        //  c
         let {BottomLeft = a; Top = b; BottomRight = c;} = triangle
+
+        let i1 = (a + c |> Y 0.25) * (a + b |> X 0.25)
+        let i2 = (a + c |> Y 0.375) * (a + b |> X 0.125)
+        let i3 = (a + c |> Y 0.5) * (a + b |> X 0.375)
+        let r1 = Rotate(C2, (a + b @ 0.5))
+
+        let leftBorder = a + i1 + i2 + i3 + (a + b @ 0.625) + (i3 % r1) + (i2 % r1) + (i1 % r1) + c
 
 
         ([], [])
-
-
-
-
-
-    // let p2 = focal =~ segArc @ 2.0/6.0 -- 0.5
-    // let p3 = focal =~ segArc @ 3.0/6.0 -- 0.5
-    // let p4 = focal =~ segArc @ 4.0/6.0 -- 0.5
-    // let p5 = focal =~ segArc @ 5.0/6.0 -- 0.5
-    // let p6 = focal =~ segArc @ 5.0/6.0 ++ 0.5
-    
-
-    
