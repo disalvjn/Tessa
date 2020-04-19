094aba6afb77c1e9122f4407d2ca4488f197d78a
Joseph DiSalvo <jdisalvo@projetech.com>
2020-04-19 15:01:39 -0400

    Continue fleshing out examples

--- src/Language.fs
+++ src/Language.fs
@@ -15,8 +15,8 @@ module Language =
         | Operated of origin: Point * operation: Operation
         | OnSegment of PointOnSegment
         | Intersection of Line * Line
-        static member toSegment p1 = Singlet(p1)
-        static member expression p1 = PointExp(p1)
+        static member ToSegment p1 = Singlet(p1)
+        static member Expression p1 = PointExp(p1)
 
     and PointOnSegment =
         | PointOnSegment of position: double * segment: Segment
@@ -27,9 +27,10 @@ module Language =
         | Concat of Segment * Segment
         | QuadraticBezier of ``from``: Point * ``to``: Point * control: Point
         | Snipped of original: Segment * cutAt: Segment
-        static member toSegment s = s
-        static member expression s = SegmentExp(s)
-        static member toLine s = ExtendSegment(s)
+        // | FocalSplit of farSegment: Segment * nearSegment: Segment * focal: Point * numberPolygons: int
+        static member ToSegment s = s
+        static member Expression s = SegmentExp(s)
+        static member ToLine s = ExtendSegment(s)
 
     // Lines are inifinite and Segments are finite
     and Line =
@@ -37,11 +38,11 @@ module Language =
         | AtX of xPosition: double * segment: Segment
         | AtY of yPosition: double * segment: Segment
         | ExtendSegment of Segment
-        static member toLine l = l
+        static member ToLine l = l
 
     and Operation =
         | Translate
-        | Glide
+        | GlideAround of pinned: Point * ``from``: Point * ``to``: Point
         | Rotate of angle: Rotation * point: Point
         | Mirror
 
@@ -49,7 +50,7 @@ module Language =
         | Up of PointOnSegment // must be on line
         | Down of PointOnSegment // must be on line
         | Segments of Segment list
-        static member expression p = PolygonExp(p)
+        static member Expression p = PolygonExp(p)
 
     and Expression =
         | PointExp of Point
@@ -60,10 +61,10 @@ module Language =
     let up location segment = Up <| PointOnSegment(location, segment)
     let down location segment = Down <| PointOnSegment(location, segment)
 
-    let inline asSegment (p:^t) = (^t: (static member toSegment: ^t -> Segment) (p))
+    let inline asSegment (p:^t) = (^t: (static member ToSegment: ^t -> Segment) (p))
     let inline (+) p q = Concat(asSegment p, asSegment q)
 
-    let inline asLine (x:^t) = (^t: (static member toLine: ^t -> Line) (x))
+    let inline asLine (x:^t) = (^t: (static member ToLine: ^t -> Line) (x))
     let inline (*) x y = Intersection(asLine x, asLine y)
 
     let (@) segment position = OnSegment(PointOnSegment(position, segment))
@@ -79,8 +80,8 @@ module Language =
     let (%) p r = Operated(p, r)
 
 
-    let inline expression (x:^t) = (^t: (static member expression: ^t -> Expression) (x))
-    let var name x = VarExp(name, expression x)
+    let inline Expression (x:^t) = (^t: (static member Expression: ^t -> Expression) (x))
+    let var name x = VarExp(name, Expression x)
 
 module PrimitiveCells = 
     open Language
@@ -100,7 +101,32 @@ module PrimitiveCells =
         Top: Point;
     }
 
-    type Builder<'a> = 'a -> Polygon list * Expression list
+    type Builder<'a> = 'a -> Segment list * Polygon list * Expression list
+
+    let splitFocal2 (focal: Point) (farBorder: Segment) (nearBorder: Segment) =
+        let segmentAt at = focal + (farBorder @ at) |-| nearBorder 
+        // let downFrom at = segmentAt at |> down 0.5
+        // let upFrom at = segmentAt at |> up 0.5
+        (segmentAt 0.33, segmentAt 0.66)
+
+    let splitFocal3 (focal: Point) (farBorder: Segment) (nearBorder: Segment) =
+        let segmentAt at = focal + (farBorder @ at) |-| nearBorder 
+        // let downFrom at = segmentAt at |> down 0.5
+        // let upFrom at = segmentAt at |> up 0.5
+        (segmentAt 0.25, segmentAt 0.5, segmentAt 0.75)
+
+    let splitFocal4 (focal: Point) (farBorder: Segment) (nearBorder: Segment) =
+        let segmentAt at = focal + (farBorder @ at) |-| nearBorder 
+        // let downFrom at = segmentAt at |> down 0.5
+        // let upFrom at = segmentAt at |> up 0.5
+        (segmentAt 0.2, segmentAt 0.4, segmentAt 0.6, segmentAt 0.8)
+
+    let splitFocal5 (focal: Point) (farBorder: Segment) (nearBorder: Segment) =
+        let segmentAt at = focal + (farBorder @ at) |-| nearBorder 
+        // let downFrom at = segmentAt at |> down 0.5
+        // let upFrom at = segmentAt at |> up 0.5
+        (segmentAt 0.166, segmentAt 0.33, segmentAt 0.499, segmentAt 0.66, segmentAt 0.813)
+
 
 
 module Examples = 
@@ -108,7 +134,7 @@ module Examples =
     open PrimitiveCells
 
     // Designing Tessellations p175
-    let buildWildOne:Builder<Square> = fun square ->
+    let buildWildOneCell:Builder<Square> = fun square ->
         let {TopLeft =a; TopRight= b; BottomRight=c; BottomLeft=d;} = square
 
         let i1 = a + b -|> (0.25, d + c) @ 0.25 
@@ -123,20 +149,14 @@ module Examples =
 
         let focal = a + d -|> (0.5, leftBorder) @ 2.0
 
-        let downFrom at = focal + (leftBorder @ at) |-| rightBorder |> down 0.5
-        let upFrom at = focal + (leftBorder @ at) |-| rightBorder |> up 0.5
+        let (s1, s2, s3, s4, s5) = splitFocal5 focal leftBorder rightBorder
+        let (p1, p2, p3, p4, p5, p6) = (down 0.5 s1, down 0.5 s2, down 0.5 s3, down 0.5 s4, down 0.5 s5, up 0.5 s5)
 
-        let p1 = downFrom <| 1.0/6.0
-        let p2 = downFrom <| 2.0/6.0
-        let p3 = downFrom <| 3.0/6.0
-        let p4 = downFrom <| 4.0/6.0
-        let p5 = downFrom <| 5.0/6.0
-        let p6 = upFrom <| 5.0/6.0
+        ([border; s1; s2; s3; s4; s5], [p1; p2; p3; p4; p5; p6], [var "i1" i1; var "i2" i2; var "i1 % r1" <| i1 % r1; var "i2 % r2" <| i2 % r2])
 
-        ([p1; p2; p3; p4; p5; p6], [var "i1" i1; var "i2" i2; var "i1 % r1" <| i1 % r1; var "i2 % r2" <| i2 % r2])
 
     // Designing Tessellations p176
-    let buildSoaringHigh:Builder<IsoscelesTriangle> =  fun triangle ->
+    let buildSoaringHighCell:Builder<IsoscelesTriangle> =  fun triangle ->
         // a
         // |\
         // |   \
@@ -152,9 +172,36 @@ module Examples =
         let i1 = (a + c |> Y 0.25) * (a + b |> X 0.25)
         let i2 = (a + c |> Y 0.375) * (a + b |> X 0.125)
         let i3 = (a + c |> Y 0.5) * (a + b |> X 0.375)
-        let r1 = Rotate(C2, (a + b @ 0.5))
+        let r1 = GlideAround(b, c, a)
 
-        let leftBorder = a + i1 + i2 + i3 + (a + b @ 0.625) + (i3 % r1) + (i2 % r1) + (i1 % r1) + c
+        let control = c + b -|> (0.5, a + b) @ 0.3
 
+        let topBorder = a + i1 + i2 + i3 + (a + b @ 0.625) + (i3 % r1) + (i2 % r1) + (i1 % r1) + c + QuadraticBezier(b, a, control % r1)
+        let lowerBorder = QuadraticBezier(c, b, control)
 
-        ([], [])
+        let focal = (a + c |> Y 1.3) * (c + b |> X 0.5)
+
+        let (s1, s2) = splitFocal2 focal topBorder lowerBorder
+        let (p1, p2, p3) = (down 0.5 s1, down 0.5 s2, up 0.5 s2)
+
+        ([topBorder + lowerBorder; s1; s2], [p1; p2; p3], [var "i1" i1; var "i2" i2; var "i3" i3; var "c" control; var "c%r1" <| control % r1])
+
+    // Designing Tessellations p71 -- Mosaic floor in Amber Palace in Jaipur, India
+    let buildTriangleMosiacCell:Builder<IsoscelesTriangle> = fun triangle ->
+        let {BottomLeft = a; Top = b; BottomRight = c;} = triangle
+
+        let down1 = (a + b @ 0.33) + (a + c @ 0.5)
+        let down2 = (c + b @ 0.33) + (c + a @ 0.5)
+        let middleUpVertex = ((a + c |> X 0.5) * (a + b |> Y 0.75))
+        let upChain = (a + c @ 0.25) + (a + b @ 0.66) + middleUpVertex + (c + b @ 0.66) + (c + a @ 0.25)
+        let finalUp = b + middleUpVertex
+
+        // Polygons on row 1
+        let (p1, p2, p3, p4) = (up 0.1 <| a + c, up 0.4 <| a + c, up 0.6 <| a + c, up 0.9 <| a + c)
+        // Row 2
+        let (p5, p6, p7) = (up 0.1 down1, up 0.9 down1, up 0.1 down2)
+        // Row 3
+        let (p8, p9) = (down 0.9 <| a + b, down 0.9 <| c + b)
+
+        ([a + b + c; down1 + down2; upChain; finalUp], [p1;p2;p3;p4;p5;p6;p7;p8;p9], [])
+
