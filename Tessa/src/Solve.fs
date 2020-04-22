4fa775ebd655255c1809e4ad6b2053c668f77c6d
Joseph DiSalvo <jdisalvo@projetech.com>
2020-04-21 22:44:51 -0400

    Refactor

--- Tessa/src/Solve.fs
+++ Tessa/src/Solve.fs
@@ -5,6 +5,7 @@ open FSharp.Collections
 open Tessa.Language 
 open System.Collections.Generic
 
+// todo: Split into more submodules. Solve.Types, Solve.Line (for line solver) etc.?
 module Solve =
     module L = Language
 
@@ -34,87 +35,76 @@ module Solve =
     type Polygon = 
         | Polygon of PolygonId * Point * Segment * PolygonDirectionality
 
-    // https://gist.github.com/tunght13488/6744e77c242cc7a94859
-    let quadraticBezierLength (orig: Point) (control: Point) (dest: Point) =
-       let ax = orig.x - 2.0 * control.x + dest.x
-       let ay = orig.y - 2.0 * control.y + dest.y
-       let bx = 2.0 * control.x - 2.0 * orig.x
-       let by = 2.0 * control.y - 2.0 * orig.y
-       let A = 4.0 * (ax * ax + ay * ay)
-       let B = 4.0 * (ax * bx + ay * by)
-       let C = bx * bx + by * by
-       let Sabc = 2.0 * sqrt(A+B+C)
-       let A_2 = sqrt(A)
-       let A_32 = 2.0 * A * A_2
-       let C_2 = 2.0 * sqrt(C)
-       let BA = B / A_2
-
-       (A_32 * Sabc + A_2 * B * (Sabc - C_2) + (4.0 * C * A - B * B) * log((2.0 * A_2 + BA + Sabc) / (BA + C_2))) / (4.0 * A_32);
-
-    let straightLength orig dest =
-        let square x = x * x
-        (square (orig.x - dest.x)) + (square (orig.y - dest.y)) |> sqrt
-
     let length segment = 
+        // https://gist.github.com/tunght13488/6744e77c242cc7a94859
+        let quadraticBezierLength (orig: Point) (control: Point) (dest: Point) =
+           let ax = orig.x - 2.0 * control.x + dest.x
+           let ay = orig.y - 2.0 * control.y + dest.y
+           let bx = 2.0 * control.x - 2.0 * orig.x
+           let by = 2.0 * control.y - 2.0 * orig.y
+           let A = 4.0 * (ax * ax + ay * ay)
+           let B = 4.0 * (ax * bx + ay * by)
+           let C = bx * bx + by * by
+           let Sabc = 2.0 * sqrt(A+B+C)
+           let A_2 = sqrt(A)
+           let A_32 = 2.0 * A * A_2
+           let C_2 = 2.0 * sqrt(C)
+           let BA = B / A_2
+
+           (A_32 * Sabc + A_2 * B * (Sabc - C_2) + (4.0 * C * A - B * B) * log((2.0 * A_2 + BA + Sabc) / (BA + C_2))) / (4.0 * A_32);
+
+        let straightLength orig dest =
+            let square x = x * x
+            (square (orig.x - dest.x)) + (square (orig.y - dest.y)) |> sqrt
+
         match segment with
             | Straight (orig, dest) -> straightLength orig dest
             | QuadraticBezier (orig, control, dest) -> quadraticBezierLength orig control dest
-
-    let segmentChainCumulativeLengths segmentChain = 
-        let rec go list cumulative m = 
-            match list with 
-                | [] -> m
-                | head::tail -> 
-                    let lengthHere = length head
-                    let newCumulative = lengthHere + cumulative
-                    go tail newCumulative <| Map.add head (cumulative, newCumulative) m
-        go segmentChain 0.0 Map.empty
-
-    let segmentAt segmentChain location =
-        let cumulatives = segmentChainCumulativeLengths segmentChain
-        let totalLength = List.sumBy length segmentChain
-        let hitAt = location * totalLength
-        segmentChain |> List.skipWhile (fun s -> Map.find s cumulatives |> snd |> (>) hitAt) |> List.tryHead
 
     let slope orig dest = 
         match dest.x - orig.x with
             | 0.0 -> None
             | _ -> Some <| (dest.y - orig.y) / (dest.x - orig.x)
 
-    let pointSlope orig dest = 
-        slope orig dest |> Option.map (fun m x -> m * (x - dest.x) + dest.y)
-
-    let evaluate orig dest x =
-        pointSlope orig dest |> Option.map (fun f -> f x)
+    let evaluateLineAt orig dest x =
+         slope orig dest 
+        |> Option.map (fun m -> m * (x - dest.x) + dest.y)
 
     let pointOnStraightSegment orig dest location = 
         let dx = dest.x - orig.x
         let dy = dest.y - orig.y
         let newX = orig.x + dx*location
-        match evaluate orig dest newX with
+        match evaluateLineAt orig dest newX with
             | None -> {x = orig.x; y = orig.y + dy*location}
             | Some y -> {x = newX; y = y}
 
     let pointOnSegmentChain (segmentChain: SegmentChain) (location: Location) : Segment * Point = 
-        let cumulatives = segmentChainCumulativeLengths segmentChain
+        let accumulate (cumulative, m) head =
+            let lengthHere = length head
+            let newCumulative = lengthHere + cumulative
+            (newCumulative, Map.add head (cumulative, newCumulative) m)
+
+        let (_, cumulatives) = List.fold accumulate (0.0, Map.empty) segmentChain
         let total = List.sumBy length segmentChain
         let hitAt = location * total
-        match segmentAt segmentChain location with
+
+        let getOnSegment segment orig dest = 
+            let (start, finish) = Map.find segment cumulatives
+            let locationOnThisSegment = (hitAt - start) / (finish - start)
+            (segment, pointOnStraightSegment orig dest locationOnThisSegment)
+
+        let choiceSegment = segmentChain |> List.skipWhile (fun s -> Map.find s cumulatives |> snd |> (>) hitAt) |> List.tryHead
+
+        match choiceSegment with
             | None -> 
                 match List.tryLast segmentChain with
                 | None -> failwith "Can't take a point on an empty segment chain"
-                | Some(Straight(orig, dest) as segment) ->
-                    // todo: this is duplicated from below
-                    let (start, finish) = Map.find segment cumulatives
-                    let locationOnThisSegment = (hitAt - start) / (finish - start)
-                    (segment, pointOnStraightSegment orig dest locationOnThisSegment)
+                | Some(Straight(orig, dest) as segment) -> getOnSegment segment orig dest
                 | Some(QuadraticBezier(_)) -> failwith "No way to extend quadratic bezier"
-                
-            | Some(Straight(orig, dest) as segment) -> 
-                let (start, finish) = Map.find segment cumulatives
-                let locationOnThisSegment = (hitAt - start) / (finish - start)
-                (segment, pointOnStraightSegment orig dest locationOnThisSegment)
+            | Some(Straight(orig, dest) as segment) -> getOnSegment segment orig dest
             | Some(QuadraticBezier(_)) -> failwith "oof"
+
+    let mergeSegmentChains chain1 chain2 = failwith ""
 
     let solveLinePerpendicular (location: Location) (segment: Segment) =
         match segment with
@@ -146,20 +136,6 @@ module Solve =
             | QuadraticBezier(_,_,_) -> failwith "oof"
             | Straight(orig, dest) -> Sloped(pointOnStraightSegment orig dest location, 0.0)
 
-
-
-
-
-
-    let mergeSegmentChains chain1 chain2 = failwith ""
-
-
-module TestIt =
-    open Solve
-    let chain = [Straight ({x = 0.0; y= 0.0;}, {x = 2.0; y = 0.0;}); Straight ({x = 2.0; y = 0.0;}, {x = 3.0; y = 1.0;})]
-
-
-    let computed = segmentAt chain 0.8
 
 
     // let test1 =  pointOnSegment (point 0.0 0.0) (point 1.0 2.0) 0.5 
